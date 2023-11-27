93fe075bf5c94e8e65c3fdd1b927ec4100371e6f
웹스토리보이 <webstupids@gmail.com>
2023-11-27 11:47:31 +0900

    first commit

---
+++ src/components/section/HeaderSection.vue
@@ -0,0 +1,114 @@
+<template>
+    <header id="header" role="banner">
+        <div class="header__inner">
+            <div class="header__nav">
+                <h1>Movie Star</h1>
+                <nav>
+                    <ul>
+                        <li><a href="#">웹쓰 영화 Top10</a></li>
+                        <li><a href="#">코딩 영화 Top10</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="header__intro"
+                style="backgroundImage:url(https://image.tmdb.org/t/p/w500/kjQBrc00fB2RjHZB3PGR4w9ibpz.jpg)">
+                <div class="container">
+                    <div class="left play__icon">
+                        <a href="#">
+                            <img src="https://image.tmdb.org/t/p/w500/vFsSluuzqxR46Ils9ib52ItdE9u.jpg" alt="dd">
+                        </a>
+                    </div>
+                    <div class="right">
+                        <h2>크리에이터</h2>
+                        <p class="desc">
+                            인류를 지키기 위해 만들어진 AI가 LA에 핵폭탄을 터뜨린 후, 인류와 AI 간의 피할 수 없는 전쟁이 시작된다. 전직 특수부대 요원 ‘조슈아’는
+                            실종된 아내의 단서를 얻을지도 모른다는 생각에 전쟁을 끝내기 위한 인류의 작전에 합류한다. 인류를 위협할 강력한 무기와 이를 창조한 ‘창조자’를 찾아 나서고, 그 무기가
+                            아이 모습의 AI 로봇 '알피'란 사실을 알게 되는데… 인간적인가, 인간의 적인가? 10월, SF 블록버스터의 신세계가 펼쳐진다!
+                        </p>
+                        <p class="date">개봉 : 2023-09-27</p>
+                        <p class="average">평점 : 7.1</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss">
+.header__inner {
+    .header__nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        h1 {
+            font-size: 20px;
+            color: var(--red);
+            font-family: 'LotteriaDdag';
+            padding: 5px 1rem;
+            margin: 20px 10px;
+            border: 1px solid var(--red);
+            display: inline-block;
+            text-transform: uppercase;
+        }
+
+        nav {
+            li {
+                display: inline;
+
+                a {
+                    display: inline-block;
+                    padding: 10px;
+                }
+            }
+        }
+    }
+
+}
+
+.header__intro {
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    position: relative;
+    padding: 30px;
+
+    &::before {
+        content: '';
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        background-color: #00000032;
+        backdrop-filter: blur(7px);
+        z-index: 1;
+    }
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+        position: relative;
+        z-index: 10;
+
+        .left {
+            width: 350px;
+
+        }
+
+        .right {
+            width: calc(100% - 390px);
+
+            h2 {
+                font-size: 30px;
+                margin-bottom: 10px;
+            }
+
+            .desc {
+                margin-bottom: 10px;
+            }
+        }
+    }
+
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,165 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import HeaderSection from '@/components/section/HeaderSection.vue';
+import FooterSection from '@/components/section/FooterSection.vue';
+
+const movies = ref([]);
+const searchKeyword = ref('');
+
+const fetchMovies = async (category) => {
+  let url = 'https://api.themoviedb.org/3/movie/popular';
+
+  switch (category) {
+    case 'latest':
+      url = 'https://api.themoviedb.org/3/movie/now_playing';
+      break;
+    case 'popular':
+      url = 'https://api.themoviedb.org/3/movie/popular'
+      break;
+    case 'upcoming':
+      url = 'https://api.themoviedb.org/3/movie/upcoming'
+      break;
+    case 'toprated':
+      url = 'https://api.themoviedb.org/3/movie/top_rated'
+      break;
+  }
+
+  try {
+    const response = await axios.get(url, {
+      params: {
+        api_key: '9278d13f704ad0fe53c2263b692efd89',
+        language: 'ko-KR',
+        page: '1'
+      }
+    });
+    movies.value = response.data.results;
+    console.log(response.data.results)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const serachMovies = async () => {
+  try {
+    const response = await axios.get('https://api.themoviedb.org/3/search/movie', {
+      params: {
+        api_key: '9278d13f704ad0fe53c2263b692efd89',
+        language: 'ko-KR',
+        query: searchKeyword.value
+      }
+    });
+    movies.value = response.data.results;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+onMounted(async () => {
+  await fetchMovies('latest');
+});
+</script>
+
+<template>
+  <HeaderSection />
+  <main id="main" role="main">
+    <div class="container">
+      <div class="movie__inner">
+
+        <section class="movie__search">
+          <h2 class="blind">검색하기</h2>
+          <input type="search" v-model="searchKeyword" placeholder="검색어를 입력해주세요!" @keyup.enter="serachMovies">
+          <button type="submit" @click="serachMovies">검색</button>
+        </section>
+        <!-- //movie__search -->
+
+        <div class="movie__tag">
+          <ul>
+            <li><a href="#" @click="fetchMovies('latest')">최신 영화</a></li>
+            <li><a href="#" @click="fetchMovies('popular')">인기 영화</a></li>
+            <li><a href="#" @click="fetchMovies('upcoming')">개봉 예정</a></li>
+            <li><a href="#" @click="fetchMovies('toprated')">최고 평점</a></li>
+          </ul>
+        </div>
+        <!-- //movie__tag -->
+
+        <section class="movie__cont">
+          <h2 class="blind">영화</h2>
+          <div class="movie play__icon" v-for="movie in movies" :key="movie.id">
+            <a href="#">
+              <img :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" :alt="movie.title">
+            </a>
+          </div>
+        </section>
+        <!-- //movie__cont -->
+
+      </div>
+    </div>
+  </main>
+  <FooterSection />
+</template>
+
+
+
+
+<style lang="scss">
+.movie__search {
+  margin: 50px 0 20px;
+  position: relative;
+
+  input {
+    border: 1px solid var(--black600);
+    padding: 1rem 2rem;
+    width: 100%;
+    border-radius: 50px;
+  }
+
+  button {
+    position: absolute;
+    right: 6px;
+    top: 6px;
+    width: 40px;
+    height: 40px;
+    background-color: var(--black);
+    color: #fff;
+    border-radius: 50%;
+    cursor: pointer;
+    font-size: 14px;
+  }
+}
+
+.movie__tag {
+  ul {
+    display: flex;
+
+    li {
+      a {
+        border: 1px solid var(--black);
+        padding: 0.5rem 1.3rem;
+        display: inline-block;
+        border-radius: 50px;
+        margin-bottom: 20px;
+        margin-right: 10px;
+        margin-top: 20px;
+
+        &:hover {
+          background-color: var(--black);
+          color: var(--white);
+        }
+      }
+    }
+  }
+}
+
+.movie__cont {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+  .movie {
+    width: 24%;
+    margin-bottom: 1.5%;
+    background-color: #ccc;
+  }
+}
+</style>
